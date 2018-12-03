53b6602a1e536261fb3169e4a604bf24d11a945a
Nandan1981 <Nandan1981>
2018-12-03 14:04:22 +0530

    Moved static files and removed dynamic content

---
+++ src/main/resources/templates/App.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import ReactDOM from 'reactDOM';
+import logo from '../images/logo.svg';
+import '../css/App.css';
+
+
+const domManipulator = document.querySelector("#Buy");
+ReactDOM.render(e(LikeButton),domManipulator);
